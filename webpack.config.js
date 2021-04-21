31a4902d133b7307beb77e0e5787ca299af816ce
Jiakan Wang <jkw@statsig.io>
2021-04-21 10:51:32 -0700

    remove all references to react native sdk

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, './dist');
-const RN_BUILD_DIR = path.resolve(__dirname, './react-native/dist');
-const RN_VANILLA_BUILD_DIR = path.resolve(
-  __dirname,
-  './react-native-vanilla/dist',
-);
 
 const baseConfig = {
   entry: __dirname + '/index.js',
@@ -17,34 +12,6 @@ const baseConfig = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
-    ],
-  },
-};
-
-const debugOptimization = {
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-    ],
-  },
-};
-
-const prodOptimization = {
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-        terserOptions: {
-          compress: {
-            drop_console: true,
-          },
-          format: {
-            comments: false,
-          },
-        },
-      }),
     ],
   },
 };
@@ -62,105 +29,36 @@ function output(prefix) {
   };
 }
 
-const debug = Object.assign(
-  {},
-  baseConfig,
-  output('dev-web'),
-  debugOptimization,
-  {
-    target: 'web',
-    mode: 'development',
+const debug = Object.assign({}, baseConfig, output('dev-web'), {
+  target: 'web',
+  mode: 'development',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
   },
-);
+});
 
-const prod = Object.assign(
-  {},
-  baseConfig,
-  output('prod-web'),
-  prodOptimization,
-  {
-    target: 'web',
-    mode: 'production',
+const prod = Object.assign({}, baseConfig, output('prod-web'), {
+  target: 'web',
+  mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+          format: {
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
-);
+});
 
-function reactNativeOutput(prefix) {
-  return {
-    output: {
-      path: RN_BUILD_DIR,
-      filename: (prefix ?? '') + 'statsig-react-native-sdk.js',
-      library: 'statsig',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      globalObject: 'this',
-      uniqueName: (prefix ?? '') + 'statsig-react-native-sdk',
-    },
-  };
-}
-
-const reactNativeDebug = Object.assign(
-  {},
-  baseConfig,
-  reactNativeOutput('dev-'),
-  {},
-  {
-    target: 'web',
-    mode: 'development',
-  },
-);
-
-const reactNativeProd = Object.assign(
-  {},
-  baseConfig,
-  reactNativeOutput(),
-  prodOptimization,
-  {
-    target: 'web',
-    mode: 'production',
-  },
-);
-
-function reactNativeVanillaOutput(prefix) {
-  return {
-    output: {
-      path: RN_VANILLA_BUILD_DIR,
-      filename: (prefix ?? '') + 'statsig-react-native-vanilla-sdk.js',
-      library: 'statsig',
-      libraryTarget: 'umd',
-      libraryExport: 'default',
-      globalObject: 'this',
-      uniqueName: (prefix ?? '') + 'statsig-react-native-vanilla-sdk',
-    },
-  };
-}
-
-const reactNativeVanillaDebug = Object.assign(
-  {},
-  baseConfig,
-  reactNativeVanillaOutput('dev-'),
-  debugOptimization,
-  {
-    target: 'web',
-    mode: 'development',
-  },
-);
-
-const reactNativeVanillaProd = Object.assign(
-  {},
-  baseConfig,
-  reactNativeVanillaOutput(),
-  prodOptimization,
-  {
-    target: 'web',
-    mode: 'production',
-  },
-);
-
-module.exports = [
-  debug,
-  prod,
-  reactNativeDebug,
-  reactNativeProd,
-  reactNativeVanillaDebug,
-  reactNativeVanillaProd,
-];
+module.exports = [debug, prod];
