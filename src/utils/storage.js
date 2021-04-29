4b5fee3d7c98b43dbc0a50e764ae7526831c4d58
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2021-04-29 11:45:13 -0700

    Try/catch window storage to fix chrome error (#49)
    * Fix storage error Failed to read the 'localStorage' property from 'Window'
    
    * try/catch initialization

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,57 +1,65 @@
 const storage = {
   init: function (asyncStorage = null) {
-    if (
-      !storage.local &&
-      typeof Storage !== 'undefined' &&
-      window != null &&
-      window.localStorage != null
-    ) {
-      storage.local = window.localStorage;
-    }
+    try {
+      if (
+        !storage.local &&
+        typeof Storage !== 'undefined' &&
+        window != null &&
+        window.localStorage != null
+      ) {
+        storage.local = window.localStorage;
+      }
 
-    if (
-      !storage.async &&
-      asyncStorage != null &&
-      typeof asyncStorage.getItem === 'function' &&
-      typeof asyncStorage.setItem === 'function' &&
-      typeof asyncStorage.removeItem === 'function'
-    ) {
-      storage.async = asyncStorage;
-    }
+      if (
+        !storage.async &&
+        asyncStorage != null &&
+        typeof asyncStorage.getItem === 'function' &&
+        typeof asyncStorage.setItem === 'function' &&
+        typeof asyncStorage.removeItem === 'function'
+      ) {
+        storage.async = asyncStorage;
+      }
+    } catch (e) {}
   },
 
   getItemAsync: function (key) {
-    if (storage.local) {
-      return Promise.resolve(storage.local.getItem(key));
-    }
-    if (storage.async) {
-      return storage.async.getItem(key);
-    }
+    try {
+      if (storage.local) {
+        return Promise.resolve(storage.local.getItem(key));
+      }
+      if (storage.async) {
+        return storage.async.getItem(key);
+      }
+    } catch (e) {}
     return Promise.resolve();
   },
 
   setItemAsync: function (key, value) {
-    if (storage.local) {
-      storage.local.setItem(key, value);
-      return Promise.resolve();
-    }
-    if (storage.async) {
-      return storage.async.setItem(key, value);
-    }
+    try {
+      if (storage.local) {
+        storage.local.setItem(key, value);
+        return Promise.resolve();
+      }
+      if (storage.async) {
+        return storage.async.setItem(key, value);
+      }
+    } catch (e) {}
     return Promise.resolve();
   },
 
   // should always resolve - fire and forget
   removeItemAsync: function (key) {
-    if (storage.local) {
-      storage.local.removeItem(key);
-      return Promise.resolve();
-    }
-    if (storage.async) {
-      return storage.async.removeItem(key).catch(() => {
+    try {
+      if (storage.local) {
+        storage.local.removeItem(key);
         return Promise.resolve();
-      });
-    }
+      }
+      if (storage.async) {
+        return storage.async.removeItem(key).catch(() => {
+          return Promise.resolve();
+        });
+      }
+    } catch (e) {}
     return Promise.resolve();
   },
 };
