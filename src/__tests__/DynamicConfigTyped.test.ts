e10968aabd12c018bb76fe72d4d67c11171e4fac
Bilal Quadri <bilalquadri92@gmail.com>
2021-12-01 01:29:45 -0800

    Accept optional type guard in DynamicConfig getter
    The behavior prior to this change was only type-safe for simple
    primitive types. Complex objects or Arrays of objects would be
    dangerously type-cast.
    
    This change addresses the issue by accepting an optional type guard
    function as a third parameter. If it is not supplied, the behavior
    remains unchanged from prior releases. If it is supplied, the property
    access is checked against the type guard prior to returning. If the type
    guard resolves to false, then the default value is returned instead.
    
    In cases where a null or undefined default value is passed and type
    guard is supplied for a non-nullable type in strict mode, the type
    should resolve to `T | null` or `T | undefined`. Similarly, wider types
    in the default will resolve to the wider type for the end value.
    Narrower types will remain as the wider type `T`.
    
    This allows for type-safe access of dynamic values (assuming the
    type guard function is well-written).

--- src/__tests__/DynamicConfigTyped.test.ts
+++ src/__tests__/DynamicConfigTyped.test.ts
@@ -20,6 +20,26 @@ describe('Verify behavior of DynamicConfig', () => {
     'default',
   );
 
+  type TestObject = {
+    key: string;
+    key2: number;
+  };
+
+  type OtherTestObject = {
+    someProp: string;
+    otherProp: number;
+  };
+
+  const isTestObject = (obj: any): obj is TestObject => {
+    return typeof obj?.key === 'string' && typeof obj?.key2 === 'number';
+  };
+
+  const isOtherTestObject = (obj: any): obj is OtherTestObject => {
+    return (
+      typeof obj?.someProp === 'string' && typeof obj?.otherProp === 'number'
+    );
+  };
+
   beforeEach(() => {
     expect.hasAssertions();
   });
@@ -36,5 +56,57 @@ describe('Verify behavior of DynamicConfig', () => {
       key: 'value',
       key2: 123,
     });
+  });
+
+  test('Test optional type guard when runtime check succeeds', () => {
+    const defaultTestObject: TestObject = {
+      key: 'default',
+      key2: 0,
+    };
+    expect(
+      testConfig.get('object', defaultTestObject, isTestObject),
+    ).toStrictEqual({
+      key: 'value',
+      key2: 123,
+    });
+  });
+
+  test('Test optional type guard default', () => {
+    const defaultOtherTestObject: OtherTestObject = {
+      someProp: 'other',
+      otherProp: 0,
+    };
+    expect(
+      testConfig.get('object', defaultOtherTestObject, isOtherTestObject),
+    ).toStrictEqual(defaultOtherTestObject);
+  });
+
+  test('Test optional type guard default when given a narrower type', () => {
+    const narrowerOtherTestObject = {
+      someProp: 'specificallyThisString',
+      otherProp: 0,
+    } as const;
+    expect(
+      testConfig.get('object', narrowerOtherTestObject, isOtherTestObject),
+    ).toStrictEqual(narrowerOtherTestObject);
+  });
+
+  test('Test optional type guard default when given a wider type', () => {
+    const widerOtherTestObject = {
+      someProp: 'Wider type than OtherTestObject',
+    };
+    expect(
+      testConfig.get('object', widerOtherTestObject, isOtherTestObject),
+    ).toStrictEqual(widerOtherTestObject);
+  });
+
+  test('Test optional type guard default when given null', () => {
+    expect(testConfig.get('object', null, isOtherTestObject)).toBeNull();
+  });
+
+  test('Test optional type guard default given undefined', () => {
+    expect(
+      testConfig.get('object', undefined, isOtherTestObject),
+    ).toBeUndefined();
   });
 });
