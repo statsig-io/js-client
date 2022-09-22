f24574497bc414bc10f1159add40fe55cdefab98
Jiakan Wang <jkw@statsig.io>
2022-09-22 14:26:43 -0700

    handle timeout in init callback

--- src/__tests__/StatsigSDKOptions.test.ts
+++ src/__tests__/StatsigSDKOptions.test.ts
@@ -77,4 +77,46 @@ describe('Test Statsig options', () => {
     expect(initSuccess).toEqual(true);
     expect(initMessage).toBeNull();
   });
+
+  test('init completion callback when it times out', async () => {
+    expect.assertions(5);
+    let initTime, initSuccess, initMessage;
+
+    global.fetch = jest.fn((url, params) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(
+          () =>
+            // @ts-ignore
+            resolve({
+              ok: true,
+              status: 200,
+              text: () => Promise.resolve(JSON.stringify({})),
+            }),
+          100,
+        );
+      });
+    });
+
+    const c = new StatsigClient(
+      'client-key',
+      { userID: 'jkw' },
+      {
+        initTimeoutMs: 10,
+        initCompletionCallback: (time, success, message) => {
+          initTime = time;
+          initSuccess = success;
+          initMessage = message;
+        },
+      },
+    );
+    await c.initializeAsync();
+
+    expect(typeof initTime).toEqual('number');
+    expect(initTime).toBeGreaterThanOrEqual(10);
+    expect(initTime).toBeLessThanOrEqual(100);
+    expect(initSuccess).toEqual(false);
+    expect(initMessage).toEqual(
+      'The initialization timeout of 10ms has been hit before the network request has completed.',
+    );
+  });
 });
