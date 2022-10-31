6b99b56bcf8bcd360b2d66d854de9922c5fb9742
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-10-31 11:38:51 -0700

    repro error

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,13 +1,13 @@
 export class StatsigUninitializedError extends Error {
   constructor(message?: string) {
     super(message ?? 'Call and wait for initialize() to finish first.');
-    Object.setPrototypeOf(this, StatsigUninitializedError.prototype);
+    // Object.setPrototypeOf(this, StatsigUninitializedError.prototype);
   }
 }
 
 export class StatsigInvalidArgumentError extends Error {
   constructor(message?: string) {
     super(message);
-    Object.setPrototypeOf(this, StatsigInvalidArgumentError.prototype);
+    // Object.setPrototypeOf(this, StatsigInvalidArgumentError.prototype);
   }
 }
