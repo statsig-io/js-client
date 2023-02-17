6a9141379b621328379c51c77e846bf32fff86d2
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2023-02-17 12:35:46 -0800

    Add option to bypass window undefined check

--- src/__tests__/StatsigClientOnServer.test.ts
+++ src/__tests__/StatsigClientOnServer.test.ts
@@ -32,4 +32,47 @@ describe('Verify behavior of StatsigClient outside of browser environment', () =
     await client.initializeAsync();
     client.shutdown();
   });
+
+  test('Client ignores window undefined if specified in options', async () => {
+    expect.assertions(7);
+
+    // @ts-ignore
+    global.fetch = jest.fn((url, params) => {
+      if (url.toString().includes('rgstr')) {
+        return Promise.resolve({ ok: true });
+      }
+      if (url.toString().includes('initialize')) {
+        return Promise.resolve({
+          ok: true,
+          text: () =>
+            Promise.resolve(
+              JSON.stringify(TestData),
+            ),
+        });
+      }
+    });
+
+    // verify window is undefined
+    expect(typeof window).toBe('undefined');
+
+    const client = new StatsigClient(
+      'client-xyz',
+      { email: 'tore@statsig.com' },
+      { ignoreWindowUndefined: true },
+    );
+
+    await client.initializeAsync();
+    // usable immediately, without an async initialize
+    expect(client.checkGate('test_gate')).toBe(false);
+    expect(client.checkGate('i_dont_exist')).toBe(false);
+    expect(client.checkGate('always_on_gate')).toBe(true);
+    expect(client.checkGate('on_for_statsig_email')).toBe(true);
+    expect(client.getConfig('test_config').get('number', 10)).toEqual(7);
+    expect(client.getConfig('test_config').getEvaluationDetails()).toEqual({
+      reason: EvaluationReason.Network,
+      time: expect.any(Number),
+    });
+
+    client.shutdown();
+  });
 });
