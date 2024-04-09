cddfc82defac6b165b7c3059c28593fcba21e8ad
kenny-statsig <111380336+kenny-statsig@users.noreply.github.com>
2024-04-09 15:18:01 -0700

    enforce prettier (#476)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,44 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'statsig-linter'],
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'statsig-linter', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'no-console': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['off', { varsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-for-in-array': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    '@typescript-eslint/no-unsafe-member-access': 'warn',
+    '@typescript-eslint/no-unsafe-return': 'warn',
+    '@typescript-eslint/no-unsafe-call': 'warn',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': ['warn', { ignoreVoid: true }],
+    '@typescript-eslint/restrict-plus-operands': 'warn',
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  ignorePatterns: [
+    '**/node_modules/*',
+    '**/dist/*',
+    '**/*.test.ts',
+    '**/__tests__/*.ts',
   ],
   overrides: [
     // Tests
